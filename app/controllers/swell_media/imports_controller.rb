099da33ba22ea74d6647517fb5a73790ec28b251
Gk <gk@gkparishphilp.com>
2014-04-24 14:20:58 -0700

    - require admin for imprts

--- app/controllers/swell_media/imports_controller.rb
+++ app/controllers/swell_media/imports_controller.rb
@@ -1,7 +1,10 @@
 module SwellMedia
 	class ImportsController < ApplicationController
+		before_filter :authenticate_user!
 
 		def create
+			authorize!( :admin, Media )
+			
 			if count = Importer.import( params[:file] )
 				set_flash "#{count} imported"
 				redirect_to :back
