173989bf2d86c5a1e9f309a6294dc3852ad3a224
Michael <meister@spacekace.com>
2014-10-08 12:29:14 -0700

    new assets

--- app/controllers/swell_media/assets_controller.rb
+++ app/controllers/swell_media/assets_controller.rb
@@ -3,6 +3,21 @@ module SwellMedia
 
 		before_filter :authenticate_user!
 
+		def new
+			@asset = Asset.new
+
+		end
+
+		def create
+
+			@asset = Asset.create( params.require( :asset ).permit( :parent_obj_id, :parent_obj_type, :use, :asset_type, :title, :description, :type, :sub_type, :status, :uploader, :uploader_cache ) )
+			@asset.user = current_user
+			@asset.save
+
+			redirect_to :back
+
+		end
+
 		def callback_create
 
 			@asset = Asset.create( params.require( :asset ).permit( :parent_obj_id, :parent_obj_type, :use, :asset_type, :title, :description, :type, :sub_type, :status ).merge({key: params[:key]}) )
