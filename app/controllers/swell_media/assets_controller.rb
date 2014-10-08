bb028dd5653c68258ccc788f12ca3371a139d7e1
Michael <meister@spacekace.com>
2014-10-08 15:10:58 -0700

    tweak

--- app/controllers/swell_media/assets_controller.rb
+++ app/controllers/swell_media/assets_controller.rb
@@ -16,9 +16,21 @@ module SwellMedia
 			@asset.save
 
 			if params[:response] == 'url'
+
 				render text: @asset.url, layout: nil
+
+			elsif request.env['HTTP_REFERER']
+
+				uri =  URI.parse(request.env['HTTP_REFERER'])
+				new_query_ar = URI.decode_www_form(uri.query) << ["asset_url", @asset.url]
+				uri.query = URI.encode_www_form(new_query_ar)
+
+				redirect_to uri.to_s
+
 			else
+
 				redirect_to :back
+
 			end
 
 
@@ -30,7 +42,18 @@ module SwellMedia
 			@asset.user = current_user
 			@asset.save
 
-			render layout: nil
+			if params[:async]
+
+				render layout: nil
+
+			elsif params[:redirect_to]
+
+				uri =  URI.parse(params[:redirect_to])
+				new_query_ar = URI.decode_www_form(uri.query) << ["asset_url", @asset.url]
+				uri.query = URI.encode_www_form(new_query_ar)
+				redirect_to uri.to_s
+
+			end
 		end
 	end
 end
