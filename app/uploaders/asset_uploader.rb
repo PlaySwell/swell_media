d90a5e15f96e066e2074bede959726e8439ce3cf
Michael <meister@spacekace.com>
2014-10-08 15:51:12 -0700

    wrap carrier wave code

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -1,36 +1,46 @@
 # encoding: utf-8
 
-class AssetUploader < CarrierWave::Uploader::Base
-	include CarrierWaveDirect::Uploader
+if defined?(CarrierWave)
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-		'assets/'
-  end
+	class AssetUploader < CarrierWave::Uploader::Base
+		include CarrierWaveDirect::Uploader if defined?(CarrierWaveDirect)
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-		@name ||= construct_new_file_name(file.extension) if original_filename
-		@name ||= construct_new_file_name()
-		@name
+		# Override the directory where uploaded files will be stored.
+		# This is a sensible default for uploaders that are meant to be mounted:
+		def store_dir
+			'assets/'
+		end
+
+		# Override the filename of the uploaded files:
+		# Avoid using model.id or version_name here, see uploader/store.rb for details.
+		def filename
+			@name ||= construct_new_file_name(file.extension) if original_filename
+			@name ||= construct_new_file_name()
+			@name
+		end
+
+		def filename=(filename)
+			@name = filename
+		end
+
+		#def extension_white_list
+		#	%w(jpg jpeg gif png)
+		#end
+
+		def construct_new_file_name(extension = nil)
+
+			fname = "#{SecureRandom.uuid}"
+
+			fname = "#{fname}.#{extension}" if !extension.blank? #&& extension_white_list.include?(extension)
+
+			fname
+		end
 	end
 
-	def filename=(filename)
-		@name = filename
+else
+
+	class AssetUploader
+
 	end
 
-	#def extension_white_list
-	#	%w(jpg jpeg gif png)
-	#end
-
-	def construct_new_file_name(extension = nil)
-
-		fname = "#{SecureRandom.uuid}"
-
-		fname = "#{fname}.#{extension}" if !extension.blank? #&& extension_white_list.include?(extension)
-
-		fname
-	end
 end
