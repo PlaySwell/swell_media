f5acfc4b458e6bcfad2d2ff162ef8a8d5c358589
Michael <meister@spacekace.com>
2014-10-09 12:23:44 -0700

    fix

--- app/models/swell_media/media.rb
+++ app/models/swell_media/media.rb
@@ -33,7 +33,7 @@ module SwellMedia
 		acts_as_taggable
 
 		def self.id_from_slug( slug )
-			return nil if slug.nil?
+			return slug if slug.nil? || !slug.is_a?(String)
 			hashid = slug.split('-').last
 			puts "id_from_slug #{slug}, #{hashid}, #{SwellMedia::HASHIDS.decrypt(hashid).first}"
 			SwellMedia::HASHIDS.decrypt(hashid).first
