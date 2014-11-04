a9e0b0f88c7776c74140a0773348b14b71832ee6
Michael <meister@spacekace.com>
2014-11-04 08:27:20 -0800

    change the twitter card depending on the associated media

--- app/controllers/swell_media/root_controller.rb
+++ app/controllers/swell_media/root_controller.rb
@@ -16,12 +16,15 @@ module SwellMedia
 
 			layout = @media.slug == 'homepage' ? 'swell_media/homepage' : "#{@media.class.name.underscore.pluralize}"
 
+			twitter_card = 'summary'
+			twitter_card = ENV['TWITTER_WITH_AVATAR_CARD'] || 'summary_large_image' unless @media.try(:avatar).blank?
+
 			if @media.slug == 'homepage'
-				set_page_meta( title: "#{ENV['APP_NAME']}", twitter: { site: ENV['TWITTER_SITE'], card: 'summary', description: ENV['TWITTER_HOMEPAGE_DESCRIPTION'] }, og: { type: 'website', image: @media.avatar } )
+				set_page_meta( title: "#{ENV['APP_NAME']}", twitter: { site: ENV['TWITTER_SITE'], card: twitter_card, description: ENV['TWITTER_HOMEPAGE_DESCRIPTION'] }, og: { type: 'website', image: @media.avatar } )
 			elsif @media.title.present?
-				set_page_meta( title: "#{@media.title} | #{ENV['APP_NAME']}", description: ActionView::Base.full_sanitizer.sanitize(@media.description), twitter: { site: ENV['TWITTER_SITE'], card: 'summary' }, og: { description: @media.subtitle, image: @media.avatar } )
+				set_page_meta( title: "#{@media.title} | #{ENV['APP_NAME']}", description: ActionView::Base.full_sanitizer.sanitize(@media.description), twitter: { site: ENV['TWITTER_SITE'], card: twitter_card }, og: { description: @media.subtitle, image: @media.avatar } )
 			else
-				set_page_meta( title: "#{@media.sanitized_content[0..128]} | #{ENV['APP_NAME']}", description: @media.content, twitter: { site: ENV['TWITTER_SITE'], card: 'summary' }, og: { description: ActionView::Base.full_sanitizer.sanitize(@media.content), image: @media.avatar } )
+				set_page_meta( title: "#{@media.sanitized_content[0..128]} | #{ENV['APP_NAME']}", description: @media.content, twitter: { site: ENV['TWITTER_SITE'], card: twitter_card }, og: { description: ActionView::Base.full_sanitizer.sanitize(@media.content), image: @media.avatar } )
 			end
 
 			begin
