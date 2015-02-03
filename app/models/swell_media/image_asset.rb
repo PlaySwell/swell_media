4dfa880c02294e05674e496793cc513046f5198c
Michael <meister@spacekace.com>
2015-02-03 11:48:19 -0800

    oops.  scope fixing

--- app/models/swell_media/image_asset.rb
+++ app/models/swell_media/image_asset.rb
@@ -1,12 +1,16 @@
-class ImageAsset < SwellMedia::Asset
+module SwellMedia
 
-	after_create :update_dimensions
+	class ImageAsset < SwellMedia::Asset
+
+		after_create :update_dimensions
 
 
-	def update_dimensions
+		def update_dimensions
 
-		dimensions = FastImage.size(self.url, :raise_on_failure=>false, :timeout=>2.0)
-		self.update(width: dimensions[0], height: dimensions[1]) if dimensions.present?
+			dimensions = FastImage.size(self.url, :raise_on_failure=>false, :timeout=>2.0)
+			self.update(width: dimensions[0], height: dimensions[1]) if dimensions.present?
+
+		end
 
 	end
 
