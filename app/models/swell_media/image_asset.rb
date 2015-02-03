cd0d8a2e22acb244dfd6151f1cf2843968d028b2
Gk <gk@gkparishphilp.com>
2015-02-03 15:17:12 -0800

    - `

--- app/models/swell_media/image_asset.rb
+++ app/models/swell_media/image_asset.rb
@@ -6,10 +6,8 @@ module SwellMedia
 
 
 		def update_dimensions
-
-			dimensions = FastImage.size(self.url, :raise_on_failure=>false, :timeout=>2.0)
-			self.update(width: dimensions[0], height: dimensions[1]) if dimensions.present?
-
+			dimensions = FastImage.size( self.url, raise_on_failure: false, timeout: 2.0 )
+			self.update( width: dimensions[0], height: dimensions[1] ) if dimensions.present?
 		end
 
 	end
