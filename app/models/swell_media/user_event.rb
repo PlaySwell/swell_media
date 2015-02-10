58569dc8b39750aceee3614e674d3354dbd44d05
Gk <gk@gkparishphilp.com>
2015-02-10 12:52:24 -0800

    - added basic charting to user_events

--- app/models/swell_media/user_event.rb
+++ app/models/swell_media/user_event.rb
@@ -15,6 +15,11 @@ module SwellMedia
 
 		### Class Methods   	--------------------------------------
 
+		def self.by_event( event )
+			return scoped if event.nil?
+			where( name: event.to_s )
+		end
+
 		def self.by_object( obj )
 			return scoped if obj.nil?
 			where( parent_obj_type: obj.class.name, parent_obj_id: obj.id )
@@ -25,7 +30,39 @@ module SwellMedia
 			where( ref_user_id: user.id )
 		end
 
+		def self.generate_daily_series( event='visit', start_date=1.month.ago, end_date=Time.zone.now )
+			start_date = start_date.beginning_of_day
+			end_date = end_date.end_of_day
+
+			query = <<-END 
+				SELECT
+					date,
+					coalesce( count, 0 ) AS count
+				FROM
+					generate_series(
+						'#{start_date}'::timestamp,
+						'#{end_date}'::timestamp,
+						'1 day') AS date
+				LEFT OUTER JOIN
+					(SELECT
+						date_trunc('day', user_events.created_at) as day,
+						count( user_events.id ) as count
+						FROM user_events
+						WHERE
+							name = '#{event}'
+							AND created_at >= '#{start_date}'
+							AND created_at <= '#{end_date}'
+						GROUP BY day) results
+					ON (date = results.day) 
+			END
+
+			return self.connection.execute( query )
+		end
+
 		def self.dated_between( start_date=1.month.ago, end_date=1.month.from_now )
+			start_date = start_date.beginning_of_day
+			end_date = end_date.end_of_day
+			
 			where( "created_at between ? and ?", start_date, end_date )
 		end
 
