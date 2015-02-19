06d7ffc6c185427d7a301a10bb29b725f4f06903
Michael <meister@spacekace.com>
2015-02-18 21:49:30 -0800

    don't attach avatar assets to objects, until they are saved

--- app/models/swell_media/media.rb
+++ app/models/swell_media/media.rb
@@ -62,9 +62,11 @@ module SwellMedia
 
 		def avatar_asset_file=( file )
 			return false unless file.present?
-			asset = ImageAsset.new(use: 'avatar', asset_type: 'image', status: 'active', parent_obj: self)
+			asset = ImageAsset.new(use: 'avatar', asset_type: 'image', status: 'active')
 			asset.uploader = file
 			asset.save
+			asset.parent_obj = self
+			assets << asset
 			self.avatar = asset.try(:url)
 		end
 
@@ -74,8 +76,13 @@ module SwellMedia
 
 		def avatar_asset_url=( url )
 			return false unless url.present?
-			asset = ImageAsset.initialize_from_url(url, use: 'avatar', asset_type: 'image', status: 'active', parent_obj: self)
-			asset.save unless asset.nil?
+			asset = ImageAsset.initialize_from_url(url, use: 'avatar', asset_type: 'image', status: 'active')
+			unless asset.nil?
+				asset.save
+
+				asset.parent_obj = self
+				assets << asset
+			end
 			puts "avatar_asset_url= asset: #{asset}"
 			self.avatar = asset.try(:url) || url
 		end
