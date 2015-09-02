b59881880ddaeb255376a037b6773dcc03cb5f37
Michael <meister@spacekace.com>
2015-09-02 14:46:05 -0700

    tweak tracking

--- app/services/swell_media/google_analytics_event_service.rb
+++ app/services/swell_media/google_analytics_event_service.rb
@@ -15,9 +15,9 @@ module SwellMedia
 					tid: SwellMedia.google_analytics_code,
 					cid: (args[:client_id] || SecureRandom.uuid),
 					t: 'event',
-					ec: args[:category] || ( user_event.parent_obj.nil? ? 'Site Event' : user_event.parent_obj.class.name ),
+					ec: args[:category] || ( user_event.parent_obj.nil? ? ( user_event.parent_controller.try(:gsub, /_/, ' ').try(:titleize).try(:singularize) || 'Site Event') : user_event.parent_obj.class.name ),
 					ea: args[:action] || user_event.name,
-					el: args[:label] || user_event.parent_obj.to_s,
+					el: args[:label] || (user_event.parent_obj || "#{user_event.parent_controller}##{user_event.parent_action}").to_s,
 					ev: args[:value] || user_event.value || 0
 			}
 
